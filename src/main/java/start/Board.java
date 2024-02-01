77d392d642b2cf7cd541991a0d90b0ee7244d761
anxi01 <tjdals2893@naver.com>
2024-02-01 18:26:20 +0900

    ch04 : AUTO 전략

--- src/main/java/start/Board.java
+++ src/main/java/start/Board.java
@@ -4,18 +4,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.TableGenerator;
 import lombok.Data;
 
 @Entity
-@TableGenerator(
-    name = "BOARD_SEQ_GENERATOR",
-    table = "MY_SEQUENCES",
-    pkColumnValue = "BOARD_SEQ", allocationSize = 1)
 @Data
 public class Board {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.TABLE, generator = "BOARD_SEQ_GENERATOR")
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
 }
