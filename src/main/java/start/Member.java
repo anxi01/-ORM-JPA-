7da96c5d93a5d7f851877510ae6294166b6de7cd
anxi01 <tjdals2893@naver.com>
2024-02-01 17:41:08 +0900

    ch04 : 다양한 매핑 사용

--- src/main/java/start/Member.java
+++ src/main/java/start/Member.java
@@ -1,9 +1,15 @@
 package start;
 
+import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -21,4 +27,16 @@ public class Member {
   private String username;
 
   private Integer age;
+
+  @Enumerated(EnumType.STRING)
+  private RoleType roleType;
+
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date createdDate;
+
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date lastModifiedDate;
+
+  @Lob
+  private String description;
 }
