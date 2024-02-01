c8a9606807307183fc7d43601881b0914fb74eef
anxi01 <tjdals2893@naver.com>
2024-02-01 18:19:39 +0900

    ch04 : SEQUENCE 전략

--- src/main/java/start/Board.java
+++ src/main/java/start/Board.java
@@ -4,13 +4,18 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import lombok.Data;
 
 @Entity
+@SequenceGenerator(
+    name = "BOARD_SEQ_GENERATOR",
+    sequenceName = "BOARD_SEQ", // 매핑할 데이터베이스 시퀀스 이름
+    initialValue = 1, allocationSize = 1)
 @Data
 public class Board {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "BOARD_SEQ_GENERATOR")
   private Long id;
 }
