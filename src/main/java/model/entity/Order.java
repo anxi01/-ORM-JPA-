6b89783f0eaa8e54c43ab827c0adc9428069c0b9
anxi01 <tjdals2893@naver.com>
2024-02-04 02:19:56 +0900

    실전예제 : Order 다대일 연관관계 매핑

--- src/main/java/model/entity/Order.java
+++ src/main/java/model/entity/Order.java
@@ -1,12 +1,17 @@
 package model.entity;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -22,12 +27,30 @@ public class Order {
   @Column(name = "ORDER_ID")
   private Long id;
 
-  @Column(name = "MEMBER_ID")
-  private Long memberId;
-
   @Temporal(TemporalType.TIMESTAMP)
   private Date orderDate; // 주문 날짜
 
   @Enumerated(EnumType.STRING)
   private OrderStatus status;
+
+  @ManyToOne
+  @JoinColumn(name = "MEMBER_ID")
+  private Member member;
+
+  @OneToMany(mappedBy = "order")
+  private List<OrderItem> orderItems = new ArrayList<>();
+
+  public void setMember(Member member) {
+    // 기존 관계 제거
+    if (this.member != null) {
+      this.member.getOrders().remove(this);
+    }
+    this.member = member; // order -> member
+    member.getOrders().add(this); // member -> order
+  }
+
+  public void addOrderItem(OrderItem orderItem) {
+    orderItems.add(orderItem); // order -> orderItem
+    orderItem.setOrder(this);// orderItem -> order
+  }
 }
