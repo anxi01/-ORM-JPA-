53362d99b3e4c0e40cdda180ee1f6db1affacb1a
anxi01 <tjdals2893@naver.com>
2024-02-04 02:19:21 +0900

    실전예제 : Member 일대다 연관관계 매핑

--- src/main/java/model/entity/Member.java
+++ src/main/java/model/entity/Member.java
@@ -1,9 +1,12 @@
 package model.entity;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import lombok.Data;
 
 @Entity
@@ -20,4 +23,7 @@ public class Member {
   private String city;
   private String street;
   private String zipcode;
+
+  @OneToMany(mappedBy = "member")
+  private List<Order> orders = new ArrayList<>();
 }
