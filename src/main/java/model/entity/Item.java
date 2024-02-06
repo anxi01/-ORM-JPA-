9e7d54dfa25eb69c8d66eb564fc00f1b1618b8f6
anxi01 <tjdals2893@naver.com>
2024-02-06 18:09:01 +0900

    실전예제 : Item - 카테고리 다대다 관계 설정

--- src/main/java/model/entity/Item.java
+++ src/main/java/model/entity/Item.java
@@ -1,9 +1,12 @@
 package model.entity;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import lombok.Data;
 
 @Entity
@@ -21,4 +24,7 @@ public class Item {
 
   // OrderItem -> Item 방향으로 참조하는 다대일 단방향 관계만 설정
   // 이유 : 주문상품에서 상품을 상품을 참조할 일은 많지만, 상품에서 주문상품을 참조할 일은 거의 없기 때문이다.
+
+  @ManyToMany(mappedBy = "items")
+  private List<Category> categories = new ArrayList<>();
 }
