0588c0c7d3bd4af048af6ff7beacb177808d275c
anxi01 <tjdals2893@naver.com>
2024-02-22 17:46:52 +0900

    실전예제 : Book - Item 상속 클래스 생성

---
+++ src/main/java/model/entity/item/Book.java
@@ -0,0 +1,12 @@
+package model.entity.item;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("B")
+public class Book extends Item{
+
+  private String author;
+  private String isbn;
+}
