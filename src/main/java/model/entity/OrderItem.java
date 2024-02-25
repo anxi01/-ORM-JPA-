3e1a8f3f0b82a9a133b4520fdb7b8892d02bb5b2
anxi01 <tjdals2893@naver.com>
2024-02-25 20:52:17 +0900

    실전예제 : OrderItem - item, order 지연 로딩 설정

--- src/main/java/model/entity/OrderItem.java
+++ src/main/java/model/entity/OrderItem.java
@@ -2,6 +2,7 @@ package model.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
@@ -23,11 +24,11 @@ public class OrderItem {
   private int orderPrice; // 주문 가격
   private int count; // 주문 수량
 
-  @ManyToOne
+  @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "ORDER_ID")
   private Order order;
 
-  @ManyToOne
+  @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "ITEM_ID")
   private Item item;
 }
