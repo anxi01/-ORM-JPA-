e3a647a87ac5d384551584e288d606e14e1315e0
anxi01 <tjdals2893@naver.com>
2024-03-18 01:47:39 +0900

    실전예제 : Address - @Embeddable 클래스 생성

---
+++ src/main/java/model/entity/Address.java
@@ -0,0 +1,18 @@
+package model.entity;
+
+import javax.persistence.Embeddable;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Embeddable
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Address {
+
+  private String city;
+  private String street;
+  private String zipcode;
+
+}
