8ec99c926c4bc9cc5b4cdb5f577b90fb2a27aa27
anxi01 <tjdals2893@naver.com>
2024-06-16 22:26:36 +0900

    part10 : fetch join

--- src/main/java/start/JpaMain.java
+++ src/main/java/start/JpaMain.java
@@ -36,29 +36,64 @@ public class JpaMain {
   private static void logic(EntityManager em) {
 
     Team teamA= new Team("팀A");
+    Team teamB = new Team("팀B");
+
     Member member1 = new Member("1번", 1);
     Member member2 = new Member("2번", 2);
     Member member3 = new Member("3번", 3);
 
     teamA.addMember(member1);
     teamA.addMember(member2);
-    teamA.addMember(member3);
+    teamB.addMember(member3);
     member1.addTeam(teamA);
     member2.addTeam(teamA);
-    member3.addTeam(teamA);
+    member3.addTeam(teamB);
 
     em.persist(teamA);
+    em.persist(teamB);
     em.persist(member1);
     em.persist(member2);
     em.persist(member3);
 
-    List<Object[]> resultList = em.createQuery(
-            "select t, m from start.Team t left join t.members m")
-        .getResultList();
+    // 엔티티 페치 조인
+    String jpql = "select m from start.Member m join fetch m.team";
+    List<Member> members = em.createQuery(jpql, Member.class).getResultList();
+    for (Member member : members) {
+      // 페치 조인으로 회원과 팀을 함께 조회해서 지연 로딩 발생 X
+      System.out.println("username: " + member.getUsername() + ", team: " + member.getTeam());
+    }
 
-    for(Object[] row : resultList) {
-      System.out.println(row[0]);
-      System.out.println(row[1]);
+    // 컬렉션 페치 조인
+    jpql = "select t from Team t join fetch t.members where t.name = '팀A'";
+    List<Team> teams = em.createQuery(jpql, Team.class).getResultList();
+    for (Team team : teams) {
+      System.out.println("team: " + team);
+
+      /**
+       * 팀A의 member1, member2가 존재하고
+       * 여기서 컬렉션 페치 조인 실행시 팀 A가 2번 조회된다.
+       *
+       * team: start.Team@3d0035d2
+       * member: Member(id=3, username=1번, age=1, team=start.Team@3d0035d2)
+       * member: Member(id=4, username=2번, age=2, team=start.Team@3d0035d2)
+       * team: start.Team@3d0035d2
+       * member: Member(id=3, username=1번, age=1, team=start.Team@3d0035d2)
+       * member: Member(id=4, username=2번, age=2, team=start.Team@3d0035d2)
+       */
+      for (Member member : team.getMembers()) {
+        System.out.println("member: " + member);
+      }
+    }
+
+    // 따라서 위의 문제를 해결하기 위해 "DISTINCT" 를 사용하여 중복을 제거한다.
+    jpql = "select distinct t from Team t join fetch t.members where t.name = '팀A'";
+    teams = em.createQuery(jpql, Team.class).getResultList();
+    for (Team team : teams) {
+      System.out.println("team: " + team);
+
+      for (Member member : team.getMembers()) {
+        System.out.println("member: " + member);
+      }
     }
   }
 }
